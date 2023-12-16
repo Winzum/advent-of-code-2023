fff1c7d3f7fe5813a4ca5e3ce77a9ec49d8e6430
Winzum <winzum93@gmail.com>
2023-12-16 18:37:03 +0100

    day 16 part 2

--- src/day_16.py
+++ src/day_16.py
@@ -3,12 +3,12 @@ from time import perf_counter
 
 
 # simulates beams over grid and returns energized tiles
-def simulate_energized_tiles(grid, example):
-    print(example)
+def simulate_energized_tiles(grid, first_beam):
     limit_y, limit_x = len(grid), len(grid[0])
     beams = set()
+    visited_mirrors = set()
     energized_tiles = set()
-    energized_tiles.add((0, 0))
+    energized_tiles.add((first_beam[0], first_beam[1]))
 
     def is_valid(x, y):
         return 0 <= x < limit_x and 0 <= y < limit_y
@@ -21,9 +21,8 @@ def simulate_energized_tiles(grid, example):
             return (direction[1], direction[0])
 
     def simulate():
-        nonlocal beams, energized_tiles
+        nonlocal beams, energized_tiles, visited_mirrors
         new_beams = set()
-
         for x, y, direction in beams:
             new_x, new_y = x + direction[0], y + direction[1]
 
@@ -35,46 +34,58 @@ def simulate_energized_tiles(grid, example):
 
                 match tile:
                     case "\\" | "/":
-                        new_direction = reflect(direction, tile)
-                        new_beams.add((new_x, new_y, new_direction))
+                        if (new_x, new_y, direction) not in visited_mirrors:
+                            new_direction = reflect(direction, tile)
+                            new_beams.add((new_x, new_y, new_direction))
+                            visited_mirrors.add((new_x, new_y, direction))
+
                     case "-":
                         if direction[0] == 0:
-                            new_beams.add((new_x, new_y, (direction[1], direction[0])))
-                            new_beams.add((new_x, new_y, (-direction[1], direction[0])))
+                            if (new_x, new_y, direction) not in visited_mirrors:
+                                new_beams.add(
+                                    (new_x, new_y, (direction[1], direction[0]))
+                                )
+                                new_beams.add(
+                                    (new_x, new_y, (-direction[1], direction[0]))
+                                )
+                                visited_mirrors.add((new_x, new_y, direction))
                         else:
                             new_beams.add((new_x, new_y, direction))
                     case "|":
                         if direction[1] == 0:
-                            new_beams.add((new_x, new_y, (direction[1], direction[0])))
-                            new_beams.add((new_x, new_y, (direction[1], -direction[0])))
+                            if (new_x, new_y, direction) not in visited_mirrors:
+                                new_beams.add(
+                                    (new_x, new_y, (direction[1], direction[0]))
+                                )
+                                new_beams.add(
+                                    (new_x, new_y, (direction[1], -direction[0]))
+                                )
+                                visited_mirrors.add((new_x, new_y, direction))
                         else:
                             new_beams.add((new_x, new_y, direction))
                     case _:
                         new_beams.add((new_x, new_y, direction))
 
-        # print(new_beams)
-        if len(new_beams) == 0:
-            quit()
         beams = new_beams
+        # print(beams)
 
     # first beams moves right in case of example, down in case of prod
-    beams.add((0, 0, (1, 0) if example else (0, 1)))
+    beams.add((first_beam))
 
     # limit the loops
-    for _ in range(limit_x * limit_y):
+    while len(beams) > 0:
         simulate()
 
     return energized_tiles
 
 
-example = False
-input_grid = read_input(16, lambda line: [x for x in line], example)
-
+# input_grid = read_input(16, lambda line: [x for x in line], example)
+input_grid = read_input(16, str, False)
 # print(input_grid)
 
 # part 1
 start_time_1 = perf_counter()
-result = simulate_energized_tiles(input_grid, example)
+result = simulate_energized_tiles(input_grid, (0, 0, (0, 1)))
 print(len(result))
 end_time_1 = perf_counter()
 execution_time_1 = end_time_1 - start_time_1
@@ -82,6 +93,20 @@ print(f"The execution time is: {execution_time_1}")
 
 # part 2
 start_time_2 = perf_counter()
+largest = []
+for y in range(len(input_grid)):
+    largest.append(len(simulate_energized_tiles(input_grid, (0, y, (1, 0)))))
+    largest.append(
+        len(simulate_energized_tiles(input_grid, (len(input_grid[y]) - 1, y, (-1, 0))))
+    )
+for x in range(len(input_grid[0])):
+    largest.append(len(simulate_energized_tiles(input_grid, (x, 0, (0, 1)))))
+    largest.append(
+        len(simulate_energized_tiles(input_grid, (x, len(input_grid) - 1, (0, -1))))
+    )
+
+# print(largest)
+print(max(largest))
 
 end_time_2 = perf_counter()
 execution_time_2 = end_time_2 - start_time_2
